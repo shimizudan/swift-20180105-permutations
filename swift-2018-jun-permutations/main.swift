ef4419cd0812ebd455c13f07c46e3d56fe0c040d
清水団 <dann@shimizudan-no-macbook-pro.local>
2018-01-05 00:49:34 +0900

    最初のコミット

--- swift-2018-jun-permutations/main.swift
+++ swift-2018-jun-permutations/main.swift
@@ -8,5 +8,35 @@
 
 import Foundation
 
-print("Hello, World!")
+//順列の関数
+func permutations( a:[Int]) -> [[Int]] {
+    var e:[[Int]] = [[]]
+    var f:[[Int]] = []
+    for i in 0...a.count-1{
+        var n:Int = 1
+        var m:Int = 1
+        for p in 0...i{
+            m = n
+            n = n * (p+1)
+        }
+        for j in 0...n-1{
+            e[j].insert(a[i], at: j/m)
+        }
+        f = e
+        for _ in 0...i{
+            e += f
+        }
+    }
+    return f
+}
+
+
+//例
+permutations(a: [1,2,3,4]).count
+
+for v in permutations(a: [1,2,3,4]){
+    print(v)
+}
+
+
 
