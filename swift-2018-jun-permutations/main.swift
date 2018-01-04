b4efe7d2a9eb5aad5398ce94d45fda5cbc09baa2
清水団 <dann@shimizudan-no-macbook-pro.local>
2018-01-05 00:41:29 +0900

    Initial Commit

---
+++ swift-2018-jun-permutations/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-2018-jun-permutations
+//
+//  Created by Shimizu Dan on 2018/01/05.
+//  Copyright © 2018年 Shimizu Dan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
